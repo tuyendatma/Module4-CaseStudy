1a85e9055d38c1d23c9bcae854d775a905ae4ae2
tuyendatma <62637890+tuyendatma@users.noreply.github.com>
2020-07-28 13:20:01 +0700

    create project

---
+++ src/main/java/com/codegym/checkinhotel/CheckInHotelApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.checkinhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckInHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckInHotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/checkinhotel/CheckInHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.checkinhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckInHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
